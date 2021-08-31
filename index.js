6f93dba45fbd55d9a925a518970248be859bc9bb
rajarshisg <rajarshisengupta50@gmail.com>
2021-09-01 03:04:58 +0530

    Set up statics

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4} = require('uuid');
 const server = require('http').Server(app);
 const PORT = process.env.PORT || 8000;
 
-
+app.use(express.static('./assets'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 app.get('/', function(req, res) {
-    return res.render('room');
+    return res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', function(req, res) {
+    return res.render('room', { roomId: req.params.room });
 })
 
 
