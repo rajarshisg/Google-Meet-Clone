8dd3505b51e28749f92e5ae0b894811c2be2d4fa
rajarshisg <rajarshisengupta50@gmail.com>
2021-09-01 02:53:26 +0530

    Set up view engine

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
 const PORT = process.env.PORT || 8000;
 
 
-app.listen(PORT, function(err) {
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.get('/', function(req, res) {
+    return res.render('room');
+})
+
+
+server.listen(PORT, function(err) {
     if(err) {
         console.log(`Error :: ${err} occured while starting the server in index.js!`);
     }
