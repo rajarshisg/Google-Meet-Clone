cf7a09206e0941fdadb7f13ee034edae1a7903ef
rajarshisg <rajarshisengupta50@gmail.com>
2021-09-02 20:08:45 +0530

    Added screen share and video calling features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,210 @@
-const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.muted = true;
+const socket = io();
+const videoGrid = document.getElementById('video-grid'); //div where our video will be loaded
 
-let myVideoStream;
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
+var myPeer = new Peer();
+
+let myVideoStream; //the video stram is stored in this variable
+const myVideo = document.createElement('video'); //div which contains the video
+let currentPeer = null;
+myVideo.muted = true;
+const peers = {};
+const names = {};
+const userName = prompt("Please enter your name", "");
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+document.getElementById('time').innerText = formatAMPM(new Date) + " | Meeting";
+setInterval(setTime, 1000);
+function setTime() {
+    document.getElementById('time').innerText = formatAMPM(new Date) + " | Meeting";
+}
+
+document.getElementById('chat-box-btn').addEventListener('click', function () {
+    if (document.getElementById('chat-box').style.display == "none") {
+        document.getElementById('chat-box').style.display = "block"
+    } else {
+        document.getElementById('chat-box').style.display = "none"
+    }
 })
 
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-        video.play();
+document.getElementById('close-chat-div').addEventListener('click', function () {
+    document.getElementById('chat-box').style.display = "none";
+})
+
+navigator.mediaDevices.getUserMedia({
+    video: true, //we want video
+    audio: true //we want audio
+}).then(stream => {
+    myVideoStream = stream; //storing the video stream returned to the myVideoStream variable
+    addVideoStream(myVideo, stream, userName); //appended my stream to 'video-grid' div
+
+    myPeer.on('call', call => {
+        call.answer(stream);
+        console.log('Hello')
+        const video = document.createElement('video');
+        let html = '<i class="fas fa-microphone"></i>'
+        video.innerHTML = html;
+        call.on('stream', userVideoStream => {
+            console.log('video displayed');
+            addVideoStream(video, userVideoStream, userName)
+        })
+        currentPeer = call;
+    })
+    socket.on('user-connected', (userId) => {
+        setTimeout(connectToNewUser, 1000, userId, userName, stream);
     });
-    videoGrid.appendChild(video);
+
+})
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id, userName);
+})
+
+function connectToNewUser(userId, userName, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream, userName);
+    })
+    call.on('close', () => {
+        video.remove();
+    })
+    peers[userId] = call
+    currentPeer = call;
 }
+
+function addVideoStream(video, stream, userName) {
+    video.srcObject = stream; //setting the source of my video
+    video.addEventListener('loadedmetadata', () => {
+        video.play(); //start to play the video once loaded
+    });
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('user-video');
+    outerDiv.appendChild(video);
+    let nameDiv = document.createElement('div');
+    nameDiv.classList.add('user-name');
+    nameDiv.innerHTML = userName + `&nbsp;&nbsp;<i class="fas fa-microphone"></i>`;
+    outerDiv.appendChild(nameDiv);
+    videoGrid.appendChild(outerDiv); //appending to 'video-grid' div
+}
+
+function muteUnmuteUser() {
+    let enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled == true) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteAudio();
+    } else {
+        myVideoStream.getAudioTracks()[0].enabled = true;
+        setMuteAudio();
+    }
+}
+
+function turnUserVideoOnOff() {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled == true) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setStopVideo()
+    } else {
+        setPlayVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+function setPlayVideo() {
+    const html = `<i class="fas fa-video"></i>`
+    document.getElementById('video-off').innerHTML = html;
+    document.getElementById('video-off').style.backgroundColor = '#434649';
+}
+
+function setStopVideo() {
+    const html = `<i class="stop fas fa-video-slash"></i>`
+    document.getElementById('video-off').innerHTML = html;
+    document.getElementById('video-off').style.backgroundColor = 'tomato';
+}
+
+function setMuteAudio() {
+    const html = `<i class="fas fa-microphone"></i>`
+    document.getElementById('mute').innerHTML = html;
+    document.getElementById('mute').style.backgroundColor = '#434649';
+}
+
+function setUnmuteAudio() {
+    const html = `<i class="unmute fas fa-microphone-slash"></i>`
+    document.getElementById('mute').innerHTML = html;
+    document.getElementById('mute').style.backgroundColor = 'tomato';
+}
+
+document.getElementById('video-off').addEventListener('click', turnUserVideoOnOff);
+document.getElementById('mute').addEventListener('click', muteUnmuteUser);
+
+$('form').submit(function (e) {
+    e.preventDefault();
+    let inputMsg = $('input').val();
+    if (inputMsg != '')
+        socket.emit('send-message', inputMsg, userName);
+    $('input').val('');
+})
+
+socket.on('recieve-message', (inputMsg, userName) => {
+    $('#messages').append(`<li><b style="font-size:.9rem">${userName}</b>&nbsp;${formatAMPM(new Date)}<br/><br/>${inputMsg}</li>`);
+    console.log('From server :: ', inputMsg);
+})
+
+const scrollToBottom = () => {
+    var d = $('#chats');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+scrollToBottom();
+
+
+var screenSharing = false
+function startScreenShare() {
+    if (screenSharing) {
+        stopScreenSharing()
+    }
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
+        screenStream = stream;
+        let videoTrack = screenStream.getVideoTracks()[0];
+        videoTrack.onended = () => {
+            console.log('Screen sharing stopped!');
+            stopScreenSharing()
+        }
+        if (myPeer) {
+            let sender = currentPeer.peerConnection.getSenders().find(function (s) {
+                return s.track.kind == videoTrack.kind;
+            })
+            sender.replaceTrack(videoTrack)
+            screenSharing = true
+        }
+        console.log(screenStream)
+    })
+}
+
+function stopScreenSharing() {
+    if (!screenSharing) return;
+    let videoTrack = myVideoStream.getVideoTracks()[0];
+    if (myPeer) {
+        let sender = currentPeer.peerConnection.getSenders().find(function (s) {
+            return s.track.kind == videoTrack.kind;
+        })
+        sender.replaceTrack(videoTrack)
+    }
+    screenStream.getTracks().forEach(function (track) {
+        track.stop();
+    });
+    screenSharing = false
+}
+
+document.getElementById('screen-share-btn').addEventListener('click', startScreenShare);
--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const express = require('express');
-const app = express();
-const { v4: uuidv4} = require('uuid');
-const server = require('http').Server(app);
-const PORT = process.env.PORT || 8000;
+const express = require('express'); //requiring express
+const app = express(); //app has all the properties of express
+const server = require('http').Server(app); //creting http server
+const io = require('socket.io')(server); //socket runs on this server
+const { v4: uuidv4 } = require('uuid'); //used to get unique room ids
+const { ExpressPeerServer } = require('peer'); //WebRTC api for real time media communication
+const PORT = process.env.PORT || 8000; //port on which server runs
 
-app.use(express.static('./assets'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.get('/', function(req, res) {
-    return res.redirect(`/${uuidv4()}`);
+const peerServer = ExpressPeerServer(server, {
+    debug: true
 });
 
-app.get('/:room', function(req, res) {
+
+app.use(express.static('./assets')); //setting up static path
+app.set('view engine', 'ejs'); //setting up view engine
+app.set('views', './views'); //setting up view path
+
+//route for index
+app.get('/', function (req, res) {
+    return res.render('home');
+})
+
+//route for new meeting --> creates a new room
+app.get('/room', function (req, res) {
+    return res.redirect(`/room/${uuidv4()}`);
+});
+
+//route for opeining a specific room
+app.get('/room/:room', function (req, res) {
     return res.render('room', { roomId: req.params.room });
 })
 
+//route for meeting ending
+app.get('/end/:room', function (req, res) {
+    return res.render('meeting_end', { roomId: req.params.room });
+})
 
-server.listen(PORT, function(err) {
-    if(err) {
+//socket handels users joining/leaving and messaging
+io.on('connection', socket => {
+    //request for joining room
+    socket.on('join-room', (roomId, userId, userName) => {
+        socket.join(roomId); //joining the mentioned room
+        socket.broadcast.to(roomId).emit('user-connected', userId, userName);
+        socket.on('send-message', (inputMsg, userName) => {
+            io.to(roomId).emit('recieve-message', inputMsg, userName);
+        })
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomId).emit('user-disconnected', userId, userName);
+        })
+    });
+});
+
+//running the server
+server.listen(PORT, function (err) {
+    if (err) {
         console.log(`Error :: ${err} occured while starting the server in index.js!`);
     }
     console.log(`Server is up and running on port ${PORT}`);
